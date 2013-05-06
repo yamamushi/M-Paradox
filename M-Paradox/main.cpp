711f52b7f13fdeda31fc6a62c5cdb0dc6685d8f8
Yamamushi <yamamushi@gmail.com>
2013-05-06 11:26:29 -0500

    Initial Commit

---
+++ M-Paradox/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  M-Paradox
+//
+//  Created by Jonathan Rumion on 5/6/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
