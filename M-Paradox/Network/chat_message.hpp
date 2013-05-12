3f1cef523ce91e558a94fc254bcefdb36c7dc7db
Yamamushi <yamamushi@gmail.com>
2013-05-12 18:59:52 -0500

    SDL Client
    Portable SDL Client

---
+++ M-Paradox/Network/chat_message.hpp
@@ -0,0 +1,14 @@
+//
+//  chat_message.hpp
+//  M-Paradox
+//
+//  Created by Jonathan Rumion on 5/12/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#ifndef M_Paradox_chat_message_hpp
+#define M_Paradox_chat_message_hpp
+
+
+
+#endif
