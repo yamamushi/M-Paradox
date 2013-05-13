98766fc22d999e0cb41100acf20e3fa90773aefe
Yamamushi <yamamushi@gmail.com>
2013-05-12 23:11:22 -0500

    Continuation of our Telnet Interface

---
+++ M-Paradox/Console/Keyboard.cpp
@@ -0,0 +1,45 @@
+//
+//  Keyboard.cpp
+//  The ASCII Project
+//
+//  Created by Jonathan Rumion on 10/16/12.
+//  Copyright (c) 2012 Jonathan Rumion. All rights reserved.
+//
+
+#include "Keyboard.h"
+
+#include <iostream>
+#include <string>
+#include "Display.h"
+
+
+
+Keyboard::Keyboard(Display *attachTo){
+    
+    attachedDisplay = attachTo;
+    
+}
+
+
+
+int Keyboard::parseKey(std::string input){
+    
+
+    if(input == "a"){
+        std::cout << "a" << std::endl;
+        return KEY_a;
+    }
+    else if(input == "\x1b[A"){
+        std::cout << "up" << std::endl;
+        return KEY_UP;
+    }
+    else if(input == ""){
+        std::cout << "none" << std::endl;
+    }
+    else{
+        return KEY_NONE;
+    }
+    return 0;
+}
+    
+
