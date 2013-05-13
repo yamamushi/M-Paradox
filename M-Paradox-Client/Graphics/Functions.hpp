f733a27ea7be92e6de9c2dd25243316a034348b5
Yamamushi <yamamushi@gmail.com>
2013-05-12 19:21:21 -0500

    Window Stuff
    Image Loading / Window Resizing

---
+++ M-Paradox-Client/Graphics/Functions.hpp
@@ -0,0 +1,20 @@
+//
+//  Functions.hpp
+//  M-Paradox
+//
+//  Created by Jonathan Rumion on 5/12/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#ifndef M_Paradox_Functions_hpp
+#define M_Paradox_Functions_hpp
+
+
+bool init();
+
+bool load_files();
+
+void clean_up();
+
+
+#endif
--- M-Paradox-Client/client_main.cpp
+++ M-Paradox-Client/client_main.cpp
@@ -10,35 +10,87 @@
 #include "SDL_image/SDL_image.h"
 #include "SDL_ttf/SDL_ttf.h"
 #include "SDL_mixer/SDL_mixer.h"
+#include "Graphics/Functions.hpp"
+#include "Graphics/Window.hpp"
+#include "Graphics/Images.hpp"
 
 
 int main( int argc, char* args[] )
 {
-    //The images
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+    //Quit flag
+    bool quit = false;
     
-    //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
+    //Initialize
+    if( init() == false )
+    {
+        return 1;
+    }
     
-    //Set up screen
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+    //Create a window
+    Window myWindow;
     
-    //Load image
-    hello = IMG_Load( "./Data/Images/Backgrounds/loading.png" );
+    //If the window failed
+    if( myWindow.error() == true )
+    {
+        return 1;
+    }
     
-    //Apply image to screen
-    SDL_BlitSurface( hello, NULL, screen, NULL );
+    //Load the files
+    if( load_files() == false )
+    {
+        return 1;
+    }
     
-    //Update Screen
-    SDL_Flip( screen );
+    //While the user hasn't quit
+    while( quit == false )
+    {
+        //While there's events to handle
+        extern SDL_Event event;
+        while( SDL_PollEvent( &event ) )
+        {
+            //Handle window events
+            myWindow.handle_events();
+            
+            //If escape was pressed
+            if( ( event.type == SDL_KEYDOWN ) && ( event.key.keysym.sym == SDLK_ESCAPE ) )
+            {
+                //Quit the program
+                quit = true;
+            }
+            
+            //If the user has Xed out the window
+            if( event.type == SDL_QUIT )
+            {
+                //Quit the program
+                quit = true;
+            }
+        }
+        
+        //If the window failed
+        if( myWindow.error() == true )
+        {
+            return 1;
+        }
+        
+        //Fill the screen white
+        extern SDL_Surface *screen;
+        SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+        
+        //Show the image centered on the screen
+        extern SDL_Surface *testing;
+        apply_surface( ( screen->w - testing->w ) / 2, ( screen->h - testing->h ) / 2, testing, screen );
+        
+        //Update the screen
+        if( SDL_Flip( screen ) == -1 )
+        {
+            return 1;
+        }
+    }
     
-    //Pause
-    SDL_Delay( 2000 );
-    
-    //Quit SDL
-    SDL_Quit();
+    //Clean up
+    clean_up();
     
     return 0;
 }
+
 
