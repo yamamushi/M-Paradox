c02f09daf4e30416918420cf8f0c274bc2c36c1a
Yamamushi <yamamushi@gmail.com>
2013-05-21 13:17:21 -0500

    Accounting

---
+++ M-Paradox/Accounting/AccountRegistration.cpp
@@ -0,0 +1,9 @@
+//
+//  AccountRegistration.cpp
+//  M-Paradox
+//
+//  Created by Jonathan Rumion on 5/21/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#include "AccountRegistration.h"
---
+++ M-Paradox/Accounting/AccountRegistration.h
@@ -0,0 +1,14 @@
+//
+//  AccountRegistration.h
+//  M-Paradox
+//
+//  Created by Jonathan Rumion on 5/21/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#ifndef __M_Paradox__AccountRegistration__
+#define __M_Paradox__AccountRegistration__
+
+#include <iostream>
+
+#endif /* defined(__M_Paradox__AccountRegistration__) */
